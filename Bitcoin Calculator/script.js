a3afcb46622b8b1986a6420fb5e08fe924a5b3da
slvkubrak22 <slvkubrak@gmail.com>
2022-09-15 12:17:11 +0300

    BTC calculator

---
+++ Bitcoin Calculator/script.js
@@ -0,0 +1,3 @@
+let currentBitcoinPrice = prompt("Enter the current Bitcoin price:");
+let dollar = prompt("Who much dollars do you want to exchange?");
+let totalBitcoin = alert(`You can buy ${dollar / currentBitcoinPrice} BTC.`);
